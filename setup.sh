4d782cddbfe613e1692a8f69dfdc5cc64010a2ab
NetworkPilgrim <142445114+NetworkPilgrim@users.noreply.github.com>
2023-11-06 16:15:03 +0100

    Fixed error with unzip command

--- setup.sh
+++ setup.sh
@@ -9,7 +9,13 @@ curl -o /usr/local/bin/hddtemp-lt https://raw.githubusercontent.com/slowpeek/hdd
 curl -o ~/serversetup.zip https://codeload.github.com/NetworkPilgrim/ServerSetup/zip/refs/heads/master
 
 #
-unzip ~/serversetup.zip "ServerSetup-master/motd/*" -d "/etc/update-motd.d/"
+rm -r /etc/update-motd.d/
+
+#
+unzip -j ~/serversetup.zip "ServerSetup-master/motd/*" -d "/etc/update-motd.d/"
+
+#
+rm ~/serversetup.zip
 
 #
 chmod -R o+rx /etc/update-motd.d/
